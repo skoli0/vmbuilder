3ffe4d3d46b9e7a1a17aaa958ab46a43849bc79f
skoli0 <sandeep.akoli@gmail.com>
2017-09-26 21:43:39 +0530

    modified argument parsing and added classes for os specific vms

--- vmbuilder.py
+++ vmbuilder.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import os
+from vmimage.vmimage import *
 __author__ = 'Sandeep Koli'
 
 def get_args():
@@ -8,7 +9,6 @@ def get_args():
     # Assign description to the help doc
     parser = argparse.ArgumentParser(description='Script retrieves schedules from a given server', add_help=True,
                                     formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=50))
-    #group = parser.add_mutually_exclusive_group()
 
     # Add arguments
     parser.add_argument('-i', '--image',
@@ -29,7 +29,7 @@ def get_args():
                         default='vmuser',
                         help='User of VM',
                         metavar='\b')
-    parser.add_argument('-p', '--password',
+    parser.add_argument('-p', '--pass',
                         required=False,
                         type=str,
                         default='vmpass',
@@ -54,17 +54,9 @@ def get_args():
                         help='ISO path to installed in VM',
                         metavar='\b')
     # Array for all arguments passed to script
-    args = parser.parse_args()
-    # Assign args to variables
-    image = args.image
-    hypervisor = args.hypervisor
-    vmuser = args.user
-    vmpass = args.password
-    os = args.os
-    arch = args.arch
-    iso = args.iso
-    # Return all variable values
-    return image, hypervisor, vmuser, vmpass, os, arch, iso
+    args = vars(parser.parse_args())
+
+    return args #image, hypervisor, vmuser, vmpass, os, arch, iso
 
 def main():
     logging.basicConfig(level=logging.INFO,
@@ -74,17 +66,10 @@ def main():
     DEFAULT_HYPERVISOR = "virtualbox"
     # Match return values from get_arguments()
     # and assign to their respective variables
-    image, hypervisor, vmuser, vmpass, vmos, vmarch, vmiso = get_args()
+    arg_dict = get_args()
+    #print(arg_dict)
 
     # Print the values
-    '''
-    print("Image type: %s" % image)
-    print("Provider: %s" % hypervisor)
-    print("VM user: %s" % vmuser)
-    print("VM password: %s" % vmpass)
-    print("VM OS: %s" % vmos)
-    print("VM Architecture: %s" % vmarch)
-    print("VM ISO: %s" % vmiso)
     '''
     logging.info("Image type: %s" % image)
     logging.info("Hypervisor: %s" % hypervisor)
@@ -93,6 +78,10 @@ def main():
     logging.info("VM OS: %s" % vmos)
     logging.info("VM Architecture: %s" % vmarch)
     logging.info("VM ISO: %s" % vmiso)
+    '''
+
+    vm = BuildVM(arg_dict)
+    vm.Build()
 
     #print(executable_in_path('packer'))
 if __name__ == '__main__':
